83d1565da3b24c4bf143f25b91bc4c8a06517c65
Fahad Asat <fahad.asat.242@my.csun.edu>
2021-10-22 16:35:30 -0700

    changed deploy commands to have global and guild

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.json');
+const { clientId, token, guildId } = require('./config.json');
 
 const commands = []
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -10,7 +10,13 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
+// const rest = new REST({ version: '9' }).setToken(token);
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+//     .then(() => console.log('Successfully registered guild application commands.'))
+//     .catch(console.error);
+
 const rest = new REST({ version: '9' }).setToken(token);
 rest.put(Routes.applicationCommands(clientId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
+    .then(() => console.log('Successfully registered global application commands.'))
     .catch(console.error);
+
